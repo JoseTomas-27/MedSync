3916f6c5ff52f6e4ec8fdb41c8ea9f434a2dabf0
Jose Tomas <torresjosetomas126@gmail.com>
2025-12-02 00:21:49 -0300

    Usar SQLite para deploy en Render

--- Gestion_turnos/settings.py
+++ Gestion_turnos/settings.py
@@ -80,14 +80,11 @@ WSGI_APPLICATION = 'Gestion_turnos.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'gestion_h',
-        'USER': 'root',
-        'PASSWORD': 'Admin'
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
